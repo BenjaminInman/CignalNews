1da8edc58ea8e87990caafe78cdc2f61d1dd0b57
BenjaminInman <ben@benjamininman.com>
2026-06-11 20:56:16 -0500

    Add files via upload

--- components/MacroTicker.js
+++ components/MacroTicker.js
@@ -1,7 +1,8 @@
-import { MACRO } from "../lib/macro";
+import { getMacro } from "../lib/macro";
 
-export default function MacroTicker() {
-  const loop = [...MACRO, ...MACRO];
+export default async function MacroTicker() {
+  const items = await getMacro();
+  const loop = [...items, ...items];
   return (
     <div className="macro" aria-label="Live US macro snapshot">
       <div className="macro-tab"><span className="pulse" /> Live</div>
